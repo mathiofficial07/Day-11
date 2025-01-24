5ab811a8d1139b7a538969ca28a7cdfc6e474eda
mathiofficial07 <mathiyazhagan907@gmail.com>
2025-01-24 19:32:22 +0530

    at

--- daytask/src/App.jsx
+++ daytask/src/App.jsx
@@ -1,18 +1,41 @@
-import React, { Suspense } from 'react';
+// import React, { Suspense } from 'react';
 
-// Lazy load the component
-const LazyComponent = React.lazy(() => import('./LazyComponent'));
+// // Lazy load the component
+// const LazyComponent = React.lazy(() => import('./LazyComponent'));
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <h1>Welcome to My App</h1>
+//       {/* Use Suspense to show a fallback while the component is loading */}
+//       <Suspense fallback={<div>Loading...</div>}>
+//         <LazyComponent />
+//       </Suspense>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import './App.css'
+import Auth from './Auth'
+import User from './User'
+
+const Authorized=Auth(User);
+
+function App(){
+  const user={name:"Mathi"};
+  const isAuth=true;
+  
+
+
   return (
-    <div>
-      <h1>Welcome to My App</h1>
-      {/* Use Suspense to show a fallback while the component is loading */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <LazyComponent />
-      </Suspense>
-    </div>
-  );
+    <>
+   <Authorized isAuth={isAuth} user={user} />
+    </>
+  )
 }
 
-export default App;
+
+
+export default App
